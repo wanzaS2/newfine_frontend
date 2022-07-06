9f142ead3d963adde3e27fca92ceb41568bf0205
wanzaS2 <moono0129@daum.net>
2022-07-06 21:54:43 +0900

    커스텀 버튼, 텍스트 인풋

---
+++ src/components/MyButton.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  ActivityIndicator,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {Fonts} from '../assets/Fonts';
+
+function MyButton({...props}) {
+  return (
+    <View style={styles.buttonZone}>
+      <Pressable
+        style={
+          props.canGoNext
+            ? StyleSheet.compose(styles.myButton, styles.myButtonActive)
+            : styles.myButton
+        }
+        disabled={props.disabled}
+        onPress={props.onPress}>
+        {props.loading ? (
+          <ActivityIndicator color="white" />
+        ) : (
+          <Text style={styles.myButtonText}>{props.text}</Text>
+        )}
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonZone: {
+    alignItems: 'center',
+  },
+  myButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  myButtonActive: {
+    backgroundColor: 'darkblue',
+  },
+  myButtonText: {
+    fontFamily: Fonts.TRRegular,
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default MyButton;
