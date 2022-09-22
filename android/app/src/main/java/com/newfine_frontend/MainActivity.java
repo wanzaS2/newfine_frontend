71541906ee861ccd350a4dd50bb740f2a6d2eda5
wanzaS2 <moono0129@daum.net>
2022-09-22 20:25:55 +0900

    안드 버전 변경

--- android/app/src/main/java/com/newfine_frontend/MainActivity.java
+++ android/app/src/main/java/com/newfine_frontend/MainActivity.java
@@ -17,24 +17,43 @@ public class MainActivity extends ReactActivity {
     return "newfine_frontend";
   }
 
+//  @Override
+//  protected void onCreate(Bundle savedInstanceState) {
+//SplashScreen.show(this, R.style.SplashScreenTheme, true);   super.onCreate(null);
+//
+////    setContentView(R.layout.activity_uncaught);
+////
+////    Tread.setDefaultUncaughtExceptionHandler(new ExceptionHandl
+//  }
+//
+//  @Override
+//  protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//
+//    setContentView(R.layout.activity_main);
+//
+//    Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler());
+//  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-SplashScreen.show(this, R.style.SplashScreenTheme, true);   super.onCreate(null);
+    SplashScreen.show(this, R.style.SplashScreenTheme, true);
+    super.onCreate(savedInstanceState);
 
-//    setContentView(R.layout.activity_uncaught);
-//
-//    Tread.setDefaultUncaughtExceptionHandler(new ExceptionHandl
+//    setContentView(R.layout.activity_main);
+
+    Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler());
   }
 
-//  public class ExceptionHandler implements Tread.UncaughtExceptionHandler {
-//    @Override
-//    public void uncaughtException(Tread t, Throwable e){
-////      Log.e(TAG, "error-------> "+ e.toString());
-//      e.printStackTrace();
-//      android.os.Process.killProcess(android.os.Process.myPid());
-//      System.exit(10);
-//    }
-//  }
+  public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
+    @Override
+    public void uncaughtException(Thread t, Throwable e){
+//      Log.e(TAG, "error-------> "+ e.toString());
+      e.printStackTrace();
+      android.os.Process.killProcess(android.os.Process.myPid());
+      System.exit(10);
+    }
+  }
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
